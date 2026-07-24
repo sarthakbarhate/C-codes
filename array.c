52d18973f95db7f381e91c7d05003c57d641e6f0
sarthakbarhate <sarthakbarhate26@gmail.com>
2026-07-24 21:17:04 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+ int i,n,sum = 0;
+ scanf("%d",&n);   
+int *arr = (int*)malloc(n * sizeof(int));
+
+ for(i=0;i<n;i++)
+ {
+   scanf("%d",&arr[i]);
+}
+ for(i=0;i<n;i++)
+{
+  sum = sum+arr[i];     
+}   
+printf("%d",sum);
+ free(arr);
+ return 0;   
+    
+    
+}
