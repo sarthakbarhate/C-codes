237a03584ee9c4a938418aaf8984713bc9260274
sarthakbarhate <sarthakbarhate26@gmail.com>
2026-07-24 21:49:42 +0530

    Update array2.c

--- array2.c
+++ array2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 int main()
 {
-    int num, arr[100], i,temp;
+    int num,i,temp;
     scanf("%d", &num);
+    
+    int arr[num];
 
     for(i = 0; i < num; i++) {
         scanf("%d", &arr[i]);
