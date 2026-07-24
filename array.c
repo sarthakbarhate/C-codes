7d08d5d61327af9278916bf5620af3e24a20b6fa
sarthakbarhate <sarthakbarhate26@gmail.com>
2026-07-24 21:56:11 +0530

    Update array.c

--- array.c
+++ array.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-#include<stdlib.h>
+
 int main()
 {
  int i,n,sum = 0;
  scanf("%d",&n);   
-int *arr = (int*)malloc(n * sizeof(int));
+int arr[num];
 
  for(i=0;i<n;i++)
  {
@@ -15,7 +15,7 @@ int *arr = (int*)malloc(n * sizeof(int));
   sum = sum+arr[i];     
 }   
 printf("%d",sum);
- free(arr);
+
  return 0;   
     
     
